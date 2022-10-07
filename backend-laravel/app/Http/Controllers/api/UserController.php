57dfccabf2cd7a3ec5f18ae97c5b42477ee881d9
ElgissioFranito <mistera.inwy@gmail.com>
2022-10-07 15:32:33 +0300

    backend done

---
+++ backend-laravel/app/Http/Controllers/api/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "email" => "required",
+            "password" => "required",
+        ]);
+
+        $user = User::create([
+            "name" => $request->name,
+            "email" =>  $request->email,
+            "password" =>  bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('api-token')->accessToken;
+
+        return ["user" => $user,
+                "token"=> $token];
+
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            "email" => "required",
+            "password" => "required",
+        ]);
+
+        $user = User::where("email","=",$request->email)->first();
+
+        if($user){
+            if (Hash::check($request->password,$user->password)){
+
+                $token = $user->createToken('api-token')->accessToken;
+
+                return ["user" => $user,
+                "token"=> $token];
+            } else {
+                return ["message"=> "mot de passe erroné"];
+            }
+
+        }else{
+            return ["message"=> "email inexistant"];
+        }
+    }
+
+    public function listuser(){
+        $users = User::All();
+
+        return [$users];
+    }
+
+    // déconnexion à l'pplication
+    public function logout()
+    {
+        Auth::user()->token()->delete();
+
+        return response()->json([
+            "Utilisateur" => "deconnexion réussi"
+        ]);
+    }
+
+}
--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+
+Route::group(["middleware" => ['auth:api']], function () {
+    Route::get('/listuser',[UserController::class,'listuser']);
+    Route::post('/logout',[UserController::class,'logout']);
+});
+
+
